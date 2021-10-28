c9034ee04b246216e74b5af2658e5e4f68e33e96
VazquezRamses <75146427+VazquezRamses@users.noreply.github.com>
2021-10-27 20:14:17 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -20,6 +20,13 @@
        <div><?php echo $row ?></div>
         <label> Bienvenido </label>
         <div></div>
+	<center> 
+		<h1> <br> <img src = "imagenes/recursos/logo.png" width="60px" height="60px">
+			<img src = "imagenes/recursos/Report.jpg" width="100px" height="60px"> 
+			<img src = "imagenes/recursos/Thing.png" width="120px" height="60px"></h1>
+		<h4>ReporThing es una plataforma en la cual se busca ayudar a mantener informadas a las personas 
+		<br> acerca de distintos sucesos peligrosos o interesantes en su entorno</h4>	
+	</center> 
         <?php require "footer.php"; ?>        
     </body>
 </html> 
