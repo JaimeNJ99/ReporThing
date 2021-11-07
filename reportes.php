19462f880128b88c3606a02b08601a9c92b87927
JaimeNJ99 <jaime.nj91@gmail.com>
2021-11-07 17:49:29 -0600

    Reportes (falta por definir los tipos)

--- reportes.php
+++ reportes.php
@@ -1,5 +1,7 @@
 <?php
     require "header.php";
+    $zona = 0;
+    $tipo = 0;
 ?>
 <html>
     <head>
@@ -15,19 +17,21 @@
             }
             .tabla{
                 border: 1px solid #000;
-                height: 400px;;
+                max-height: 500px;
+                height: auto;
                 width: 25%;
                 margin-right: 20px;
                 display: inline-block;
-                overflow: scroll;
+                overflow-y: scroll;
             }
             .categorias{
                 border: 1px solid #000;
-                height:400px;
+                height: auto;
+                max-height:500px;
                 width: 200px;
                 margin-right: 10px;
                 display: inline-block;
-                overflow: scroll;
+                overflow-y: scroll;
             }
             .entrada{
                 text-align: center;
@@ -43,21 +47,81 @@
         <div class="centro">
             <div class="tabla">
                 <div class="entrada" style="background-color:steelblue"><h1>Reportes recientes</h1></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
+                <?php 
+                    $sql = "SELECT * FROM reportes ORDER BY id_reporte DESC LIMIT 10";
+                    $res = mysqli_query($conn, $sql);
+                    $row = mysqli_num_rows($res);
+                    if($row == 0){ ?>
+                        <div class="entrada">
+                            <b>Todavia no hay reportes </b>
+                        </div>
+                    <?php }else{
+                        for($i = 0; $i < $row; $i++){
+                            $consulta = mysqli_fetch_row($res);
+                ?>
+                <div class="entrada">
+                    <b>Nombre del reporte <?php echo $consulta[1]; ?>
+                    <br>Ubicación <?php echo $consulta[3]; ?>
+                    <br>Descripción <?php echo $consulta[4]; ?>
+                    </b>
+                </div>
+                <?php     
+                } }
+                mysqli_free_result($res);
+                ?>
             </div>
             <div class="tabla">
                 <div class="entrada" style="background-color:steelblue"><h1>Más votados</h1></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
+                <?php 
+                    $sql = "SELECT * FROM reportes WHERE id_reporte =
+                    (SELECT id_reporte FROM rating ORDER BY calificacion DESC LIMIT 10)";
+                    $res = mysqli_query($conn, $sql);
+                    $row = mysqli_num_rows($res);
+                    if($row == 0){ ?>
+                        <div class="entrada">
+                            <b>Todavia no se ha calificado algun reporte</b>
+                        </div>
+                    <?php }else{
+                        for($i = 0; $i < $row; $i++){
+                            $consulta = mysqli_fetch_row($res);
+                ?>
+                <div class="entrada">
+                    <b>Nombre del reporte <?php echo $consulta[1]; ?>
+                    <br>Ubicación <?php echo $consulta[3]; ?>
+                    <br>Descripción <?php echo $consulta[4]; ?>
+                    </b>
+                </div>
+                <?php     
+                } }
+                mysqli_free_result($res);
+                ?>
             </div>
             <div class="tabla">
                 <div class="entrada" style="background-color:steelblue"><h1>Cerca a tu zona</h1></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
+                <?php 
+                    $sql = "SELECT * FROM reportes WHERE id_reporte = 
+                    (SELECT id_reporte FROM zona WHERE id_zona = $zona 
+                    ORDER BY id_reporte DESC LIMIT 10)";
+                    $res = mysqli_query($conn, $sql);
+                    $row = mysqli_num_rows($res);
+                    if($row == 0){ ?>
+                        <div class="entrada">
+                            <b>Todavia no hay reportes cerca de ti</b>
+                        </div>
+                    <?php }else{
+                        for($i = 0; $i < $row; $i++){
+                            $consulta = mysqli_fetch_row($res);
+                ?>
+                <div class="entrada">
+                    <b>Nombre del reporte <?php echo $consulta[1]; ?>
+                    <br>Ubicación <?php echo $consulta[3]; ?>
+                    <br>Descripción <?php echo $consulta[4]; ?>
+                    </b>
+                </div>
+                <?php     
+                } }
+                mysqli_free_result($res);
+                ?>
             </div>
         </div>
 
@@ -66,33 +130,138 @@
         <div class="centro">
         <div class="categorias">
                 <div class="entrada" style="background-color:steelblue"><h1>categoria</h1></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
+                <?php 
+                    $sql = "SELECT * FROM reportes
+                    WHERE tipo = $tipo 
+                    ORDER BY id_reporte DESC LIMIT 10";
+                    $res = mysqli_query($conn, $sql);
+                    $row = mysqli_num_rows($res);
+                    if($row == 0){ ?>
+                        <div class="entrada">
+                            <b>Todavia no hay reportes </b>
+                        </div>
+                    <?php }else{
+                        for($i = 0; $i < $row; $i++){
+                            $consulta = mysqli_fetch_row($res);
+                ?>
+                <div class="entrada">
+                    <b>Nombre del reporte <?php echo $consulta[1]; ?>
+                    <br>Ubicación <?php echo $consulta[3]; ?>
+                    <br>Descripción <?php echo $consulta[4]; ?>
+                    </b>
+                </div>
+                <?php     
+                } }
+                mysqli_free_result($res);
+                ?>
             </div>
             <div class="categorias">
                 <div class="entrada" style="background-color:steelblue"><h1>categoria</h1></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
-            </div>
-            <div class="categorias">
-                <div class="entrada"style="background-color:steelblue"><h1>categoria</h1></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
+                <?php 
+                    $sql = "SELECT * FROM reportes
+                    WHERE tipo = $tipo 
+                    ORDER BY id_reporte DESC LIMIT 10";
+                    $res = mysqli_query($conn, $sql);
+                    $row = mysqli_num_rows($res);
+                    if($row == 0){ ?>
+                        <div class="entrada">
+                            <b>Todavia no hay reportes </b>
+                        </div>
+                    <?php }else{
+                        for($i = 0; $i < $row; $i++){
+                            $consulta = mysqli_fetch_row($res);
+                ?>
+                <div class="entrada">
+                    <b>Nombre del reporte <?php echo $consulta[1]; ?>
+                    <br>Ubicación <?php echo $consulta[3]; ?>
+                    <br>Descripción <?php echo $consulta[4]; ?>
+                    </b>
+                </div>
+                <?php     
+                } }
+                mysqli_free_result($res);
+                ?>
             </div>
             <div class="categorias">
                 <div class="entrada" style="background-color:steelblue"><h1>categoria</h1></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
+                <?php 
+                    $sql = "SELECT * FROM reportes
+                    WHERE tipo = $tipo 
+                    ORDER BY id_reporte DESC LIMIT 10";
+                    $res = mysqli_query($conn, $sql);
+                    $row = mysqli_num_rows($res);
+                    if($row == 0){ ?>
+                        <div class="entrada">
+                            <b>Todavia no hay reportes </b>
+                        </div>
+                    <?php }else{
+                        for($i = 0; $i < $row; $i++){
+                            $consulta = mysqli_fetch_row($res);
+                ?>
+                <div class="entrada">
+                    <b>Nombre del reporte <?php echo $consulta[1]; ?>
+                    <br>Ubicación <?php echo $consulta[3]; ?>
+                    <br>Descripción <?php echo $consulta[4]; ?>
+                    </b>
+                </div>
+                <?php     
+                } }
+                mysqli_free_result($res);
+                ?>
             </div>
             <div class="categorias">
                 <div class="entrada" style="background-color:steelblue"><h1>categoria</h1></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
-                <div class="entrada"><b>Nombre del reporte<br>ubicacion <br> fecha y hora</b></div>
+                <?php 
+                    $sql = "SELECT * FROM reportes
+                    WHERE tipo = $tipo 
+                    ORDER BY id_reporte DESC LIMIT 10";
+                    $res = mysqli_query($conn, $sql);
+                    $row = mysqli_num_rows($res);
+                    if($row == 0){ ?>
+                        <div class="entrada">
+                            <b>Todavia no hay reportes </b>
+                        </div>
+                    <?php }else{
+                        for($i = 0; $i < $row; $i++){
+                            $consulta = mysqli_fetch_row($res);
+                ?>
+                <div class="entrada">
+                    <b>Nombre del reporte <?php echo $consulta[1]; ?>
+                    <br>Ubicación <?php echo $consulta[3]; ?>
+                    <br>Descripción <?php echo $consulta[4]; ?>
+                    </b>
+                </div>
+                <?php     
+                } }
+                mysqli_free_result($res);
+                ?>
+            </div>
+            <div class="categorias">
+                <div class="entrada" style="background-color:steelblue"><h1>categoria</h1></div>
+                <?php 
+                    $sql = "SELECT * FROM reportes
+                    WHERE tipo = $tipo 
+                    ORDER BY id_reporte DESC LIMIT 10";
+                    $res = mysqli_query($conn, $sql);
+                    $row = mysqli_num_rows($res);
+                    if($row == 0){ ?>
+                        <div class="entrada">
+                            <b>Todavia no hay reportes </b>
+                        </div>
+                    <?php }else{
+                        for($i = 0; $i < $row; $i++){
+                            $consulta = mysqli_fetch_row($res);
+                ?>
+                <div class="entrada">
+                    <b>Nombre del reporte <?php echo $consulta[1]; ?>
+                    <br>Ubicación <?php echo $consulta[3]; ?>
+                    <br>Descripción <?php echo $consulta[4]; ?>
+                    </b>
+                </div>
+                <?php     
+                } }
+                mysqli_free_result($res);
+                ?>
             </div>
         </div>
         <?php require "footer.php" ?>
